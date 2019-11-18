9fb20e6c5504394e077d6d80d5bf9083efebaba5
Pietro Malky <pietromalky@gmail.com>
2019-11-17 19:20:31 -0600

    added idle, success and loading statuses to NewTrip.jsx

--- src/components/NewTrip.jsx
+++ src/components/NewTrip.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
+import carColored from "../assets/carColored.png";
+import home from "../assets/home.png";
 import "./styles/NewTrip.css";
 
 class NewTrip extends React.Component {
@@ -8,17 +9,18 @@ class NewTrip extends React.Component {
     super(props);
     this.state = { imageURL: "" };
     this.handleUploadImage = this.handleUploadImage.bind(this);
+    this.renderUpload = this.renderUpload.bind(this);
   }
 
   handleUploadImage(ev) {
+    this.setState({
+      imageURL: "loading"
+    });
+
     ev.preventDefault();
 
     const data = new FormData();
     data.append("file", this.uploadInput.files[0]);
-
-    this.setState({
-      imageURL: URL.createObjectURL(this.uploadInput.files[0])
-    });
 
     fetch("http://10.35.42.221:5000/api/submit", {
       method: "POST",
@@ -26,44 +28,88 @@ class NewTrip extends React.Component {
       body: data
     })
       .then(res => {
-        alert("trip video uploaded successfully");
+        console.log("success");
+        this.setState({
+          imageURL: "success"
+        });
       })
       .catch(err => {
-        alert("error when uploading your trip");
+        // alert("error when uploading your trip");
         console.error(err);
       });
   }
 
+  renderUpload() {
+    if (this.state.imageURL === "") {
+      return (
+        <>
+          <form onSubmit={this.handleUploadImage}>
+            <div className="newtrip">
+              <input
+                id="file-upload"
+                ref={ref => {
+                  this.uploadInput = ref;
+                }}
+                type="file"
+                accept="video/*"
+                capture="camera"
+                required
+              />
+              <button>Submit</button>
+              <div>
+                {this.state.imageURL === "" ? null : (
+                  <p>Uploaded {this.state.imageURL}</p>
+                )}
+              </div>
+            </div>
+          </form>
+          <Link to="/">
+            <button>
+              <img src={home}></img>
+            </button>
+          </Link>
+        </>
+      );
+    } else if (this.state.imageURL === "loading") {
+      return (
+        <>
+          <h1 style={{ color: "white" }}>
+            Uploading your video, please hang on ...
+          </h1>
+        </>
+      );
+    } else if (this.state.imageURL === "success") {
+      return (
+        <>
+          <h1 style={{ color: "white" }}>Video uploaded successfully</h1>
+          <button
+            onClick={() => {
+              this.setState({
+                imageURL: ""
+              });
+            }}
+          >
+            Upload new trip
+          </button>
+          <Link to="/">
+            <button>
+              <img src={home}></img>
+            </button>
+          </Link>
+        </>
+      );
+    }
+  }
+
   render() {
     return (
-      <form onSubmit={this.handleUploadImage}>
-        <div className="newtrip">
+      <div className="new-trip-container">
+        <div className="new-trip-top">
           <h1>Record New Trip</h1>
-          <input
-            id="file-upload"
-            ref={ref => {
-              this.uploadInput = ref;
-            }}
-            type="file"
-            accept="video/*"
-            capture="camera"
-            required
-          />
-          <button>Submit</button>
-          <div>
-            {this.state.imageURL === "" ? null : (
-              <img
-                src={this.state.imageURL}
-                alt="vid"
-                className="trip-video-container"
-              />
-            )}
-          </div>
-          <Link to="/">
-            <button>Home</button>
-          </Link>
+          <img src={carColored}></img>
         </div>
-      </form>
+        <div className="new-trip-bottom">{this.renderUpload()}</div>
+      </div>
     );
   }
 }
