5367c5c58dc15b45a9ff5e632b211bc89d2945de
Pietro Malky <pietromalky@gmail.com>
2019-11-22 14:41:52 -0600

    can fetch videos now

--- src/components/History.jsx
+++ src/components/History.jsx
@@ -4,9 +4,61 @@ import { Link } from "react-router-dom";
 import calendar from "../assets/calendar.png";
 import home from "../assets/home.png";
 
+import { serverUrl } from "../Config";
+
 import "./styles/History.css";
 
 class History extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      videoBlobs: []
+    };
+
+    this.fetchVideoList = this.fetchVideoList.bind(this);
+  }
+
+  fetchVideoList() {
+    const email = localStorage.getItem("email");
+    fetch(serverUrl + "/api/history", { method: "POST", body: email })
+      .then(res => {
+        return res.json();
+      })
+      .then(data => {
+        return data.videos;
+      })
+      .then(videoNames => {
+        videoNames.map(vid => {
+          return fetch(serverUrl + "/api/getvideo", {
+            method: "POST",
+            body: vid
+          })
+            .then(res => {
+              return res.blob();
+            })
+            .then(data => {
+              this.setState({
+                videoBlobs: [
+                  ...this.state.videoBlobs,
+                  URL.createObjectURL(data)
+                ]
+              });
+            })
+            .catch(err => {
+              console.error(err);
+            });
+        });
+      })
+      .catch(err => {
+        console.error(err);
+      });
+  }
+
+  componentDidMount() {
+    this.fetchVideoList();
+  }
+
   render() {
     return (
       <div className="history-container">
@@ -15,6 +67,22 @@ class History extends React.Component {
         </div>
         <div className="history-bottom">
           <h1>Your Trip History</h1>
+
+          <div className="history-video-container">
+            {this.state.videoBlobs.map(vid => {
+              return (
+                <video
+                  key={Math.random() * 1000}
+                  src={vid}
+                  loop
+                  style={{ height: "100px" }}
+                  muted
+                  autoPlay
+                ></video>
+              );
+            })}
+          </div>
+
           <Link to="/main">
             <button>
               <img src={home} alt="Home"></img>
