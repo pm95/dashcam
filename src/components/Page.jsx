937ed0366e7ec2a2d4741a2f9ffbe96fdbd0b59b
Pietro Malky <pietromalky@gmail.com>
2019-12-02 23:08:56 -0600

    made page functional component for lighter rendering

--- src/components/Page.jsx
+++ src/components/Page.jsx
@@ -4,27 +4,21 @@ import home from "../assets/home.png";
 
 import "./styles/Page.css";
 
-export default class Page extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <div className="page-container">
-        <div className="page-top">
-          <h1>{this.props.pageTitle}</h1>
-          <img src={this.props.pageImgSrc} alt="Page Img"></img>
-        </div>
-        <div className="page-bottom">
-          {this.props.children}
-          <Link to="/main">
-            <button>
-              <img src={home} alt="Home"></img>
-            </button>
-          </Link>
-        </div>
+export default function Page(props) {
+  return (
+    <div className="page-container">
+      <div className="page-top">
+        <h1>{props.pageTitle}</h1>
+        <img src={props.pageImgSrc} alt="Page Img"></img>
       </div>
-    );
-  }
+      <div className="page-bottom">
+        {props.children}
+        <Link to="/main">
+          <button>
+            <img src={home} alt="Home"></img>
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
 }
